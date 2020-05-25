5f2955e16ebdef390e72728132693f3e5d652cb1
Charles Delannoy <charles.delannoy@hei.fr>
2020-05-25 16:04:28 +0200

    user migration done

--- db/migrate/20200525135959_add_first_name_last_name_and_birthdate_to_users.rb
+++ db/migrate/20200525135959_add_first_name_last_name_and_birthdate_to_users.rb
@@ -1,6 +1,5 @@
-class AddEmailFirstNameLastNameAndBirthdateToUsers < ActiveRecord::Migration[6.0]
+class AddFirstNameLastNameAndBirthdateToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :email, :string
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
     add_column :users, :birthdate, :date
