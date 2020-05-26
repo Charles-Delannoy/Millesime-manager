95300b0a3630f0730cc6294ba1a874eabe70236d
lena jakubowicz <lenajaku@gmail.com>
2020-05-26 12:04:42 +0200

    new and create cave controller

--- app/controllers/caves_controller.rb
+++ app/controllers/caves_controller.rb
@@ -4,5 +4,28 @@ class CavesController < ApplicationController
     @caves = Cave.all
   end
 
+  def new
+    @cave = Cave.new
+  end
+
+  def create
+    @cave = Cave.new(cave_params)
+    @cave.user = current_user
+    if @cave.save
+      redirect_to caves_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_cave
+    @cave = Cave.find(params[:id])
+  end
+
+  def cave_params
+    params.require(:cave).permit(:name)
+  end
 end
 
