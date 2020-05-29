52efbf04f8536832ced529e352c2607518f887e6
Charles Delannoy <charles.delannoy@hei.fr>
2020-05-29 09:19:48 +0200

    seed correction

--- db/seeds.rb
+++ db/seeds.rb
@@ -78,8 +78,8 @@ base_html_doc.search('.ns-LayerMenu-link').first(10).each do |url|
         p 'apogee'
         best_year = html_doc.search('.ns-ProductGrappe-value--level')[0].text.strip.to_i
         if best_year == 0
-          p apogee_start = millesime - 3
-          p apogee_end = millesime + 3
+          p apogee_start = millesime + 3
+          p apogee_end = millesime + 6
         else
           year_diff = best_year - millesime
           p apogee_start = best_year - year_diff/2
