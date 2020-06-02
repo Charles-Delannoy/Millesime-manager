1ad49d024c58daefc4fcf45494e969e7950e573e
Charles Delannoy <charles.delannoy@hei.fr>
2020-06-02 15:46:28 +0200

    affichage dynamique des filtres selectionnés

--- app/javascript/plugins/dynamic_filter.js
+++ app/javascript/plugins/dynamic_filter.js
@@ -1,22 +1,27 @@
 const dynaFilter = () => {
   const region = document.getElementById('region-filter');
   if (region) {
+    const firstRegionValue = Number.parseInt(region.value, 10);
     const myAppelations = document.getElementById('region-value');
     const appelations = JSON.parse(myAppelations.dataset.appelations);
     const appelationSelect = document.getElementById('appelation-filter');
+    setAppelations(appelations, appelationSelect, region);
     region.addEventListener("change", (event) => {
-      const regionValue = Number.parseInt(region.value, 10);
-      const filteredApp = appelations.filter(appelation => {
-        return appelation.region_id == regionValue;
-      });
-      console.log(filteredApp);
-      appelationSelect.innerHTML = `
-      <option value="">Selectionner une appelation</option>\n
-      ${buildOptions(filteredApp)};
-      `
+      setAppelations(appelations, appelationSelect, region);
     });
   }
 };
+
+const setAppelations = (appelations, appelationSelect, region) => {
+  const regionValue = Number.parseInt(region.value, 10);
+  const filteredApp = appelations.filter(appelation => {
+    return appelation.region_id == regionValue;
+  });
+  appelationSelect.innerHTML = `
+  <option value="">Selectionner une appelation</option>\n
+  ${buildOptions(filteredApp)};
+  `
+}
 
 const buildOptions = (data) => {
   return data.map(appelation => {
