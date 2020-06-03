d520abbe2ead2bf73b2e77f24d267a713b6b4bb8
Isabelle Lafont <isalafont@gmail.com>
2020-06-03 19:34:06 +0200

    work in progress JS

--- app/javascript/plugins/active_filter.js
+++ app/javascript/plugins/active_filter.js
@@ -1,2 +1,15 @@
 const activeFilter = () => {
   const footerElement = document.querySelectorAll('.footer-item');
+  footerElement.forEach(element => {
+    element.addEventListener('click', (event) => {
+      const currentId = event.currentTarget.id;
+      const currentActive = document.getElementById('active-item');
+      const myTarget = document.getElementById(currentId);
+      console.log(myTarget);
+      myTarget.id = 'active-item';
+      console.log(myTarget.id);
+    });
+  });
+};
+
+export { activeFilter };
