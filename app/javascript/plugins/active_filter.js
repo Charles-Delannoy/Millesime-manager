e59a4998d8ae9ebc79ff66f1357fec455bac18e1
Isabelle Lafont <isalafont@gmail.com>
2020-06-04 10:54:08 +0200

    install gem svg

--- app/javascript/plugins/active_filter.js
+++ app/javascript/plugins/active_filter.js
@@ -2,12 +2,18 @@ const activeFilter = () => {
   const footerElement = document.querySelectorAll('.footer-item');
   footerElement.forEach(element => {
     element.addEventListener('click', (event) => {
-      const currentId = event.currentTarget.id;
-      const currentActive = document.getElementById('active-item');
-      const myTarget = document.getElementById(currentId);
-      console.log(myTarget);
-      myTarget.id = 'active-item';
-      console.log(myTarget.id);
+      event.preventDefault();
+      // const currentId = event.currentTarget.id;
+      const currentActive = document.querySelector('.active-item');
+      // const myTarget = document.getElementById(currentId);
+      console.log(event.currentTarget);
+      if (currentActive) {
+        currentActive.classList.remove('active-item');
+      }
+      // console.log();
+      event.currentTarget.classList.add('active-item');
+      console.log(event.currentTarget);
+
     });
   });
 };
