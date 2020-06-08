54df3b0e0fb51d070a8e1671f91f25730aeb8199
Charles Delannoy <charles.delannoy@hei.fr>
2020-06-08 07:16:44 +0200

    land on profile page if logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    redirect_to profiles_path unless current_user.nil?
   end
 end
