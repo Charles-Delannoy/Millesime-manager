b89b40f23f2b5f219b10a1c46914f88598d92997
charles delannoy <charles.delannoy@hei.fr>
2021-02-26 14:46:16 +0100

    index without login

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,4 +1,5 @@
 class WinesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
 
   def index
     @cave = Cave.find(params[:cave_id]) if params[:cave_id].present?
